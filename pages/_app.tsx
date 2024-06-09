a5aed0bb14852de09f5489d4be0b374577de7794
Ludovic Mermod <ludovic.mermod@gmail.com>
2024-06-09 21:43:35 +0200

    chore(dev): add pre-commit hooks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Component {...pageProps} />
-  );
+  return <Component {...pageProps} />;
 };
 
 export default App;
