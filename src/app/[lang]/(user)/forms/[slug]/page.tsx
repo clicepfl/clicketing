a19b39b78836c61f2f3ac64f59e0690ea8913ab9
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-08-01 16:25:40 +0200

    fix: create only one prisma client

--- src/app/[lang]/(user)/forms/[slug]/page.tsx
+++ src/app/[lang]/(user)/forms/[slug]/page.tsx
@@ -1,19 +1,17 @@
 'use server';
 
-import { PrismaClient } from '@prisma/client';
 import { Simple } from './simple';
 import { notFound } from 'next/navigation';
 import { cleanTranslations, directus } from '../../../../../directus/directus';
 import { readItem } from '@directus/sdk';
+import { prisma } from '../../../../../prisma';
 
 export default async function Page({
   params,
 }: {
   params: { slug: string; locale: string };
 }) {
-  const prisma = new PrismaClient();
   const event = await prisma.event.findUnique({ where: { slug: params.slug } });
-
   const news = await directus().request(
     readItem('news', event.directusId, {
       // @ts-ignore
