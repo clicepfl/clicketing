262e1e8a77179802687331831ce8d0f935f8a2c9
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-08-10 16:46:16 +0200

    refactor: modularize event form

--- src/db/events.ts
+++ src/db/events.ts
@@ -9,31 +9,14 @@ function convertNameToSlug(name: string) {
   return name.replaceAll(/[_ ]/g, '-').toLocaleLowerCase();
 }
 
-export async function createEvent(event: {
-  name: string;
-  slug: string;
-  directusId: number;
-  startTime: string;
-  endTime: string;
-}): Promise<ApiResult<Event>> {
+export async function createEvent(event: Event): Promise<ApiResult<Event>> {
   if (!(await hasValidAdminSession())) {
     return Err(ApiError.Forbidden);
   }
 
-  console.log(event.startTime, typeof event.startTime);
-
   try {
     const e: Event = await prisma.event.create({
-      data: {
-        name: event.name,
-        slug: event.slug || convertNameToSlug(event.name),
-        directusId: event.directusId.toString(),
-        mailTemplate: '',
-        eventStartTime: new Date(event.startTime).toISOString(),
-        eventEndTime: new Date(event.endTime).toISOString(),
-        staffingTimeSlotSize: 30,
-        data: {},
-      },
+      data: event,
     });
 
     return Ok(e);
--- src/utils.ts
+++ src/utils.ts
@@ -103,3 +103,17 @@ export enum ApiError {
   Forbidden = 'forbidden',
   Internal = 'internal',
 }
+
+/**
+ * Converts a date to the format `YYYY-MM-DDThh:mm`.
+ * This is useful to provide a `Date` object as value of a `datetime-local` input tag.
+ */
+export function convertToDateTimeLocalString(date: Date): string {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
+}
