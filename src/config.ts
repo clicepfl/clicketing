f9df4758301aa7ad146ff1aee943c752e476e394
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-10-16 15:39:38 +0200

    refactor: remove unused env variables

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,8 @@
 import { exit } from 'process';
 
 export const JWT_SECRET = process.env.JWT_SECRET;
-export const ADMIN_TOKEN = process.env.ADMIN_TOKEN;
 export const DIRECTUS_TOKEN = process.env.DIRECTUS_TOKEN;
 export const SESSION_LIFE = parseInt(process.env.SESSION_LIFE) | (60 * 60 * 24); // 1 day
-
-export const MAIL = {
-  transporter: {
-    host: process.env.MAIL_HOST,
-    port: parseInt(process.env.MAIL_PORT),
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASSWORD,
-    },
-  },
-  from: process.env.MAIL_FROM,
-};
 
 function checkVariable(name: string) {
   if (!process.env[name]) {
@@ -30,16 +17,7 @@ function checkVariable(name: string) {
  * Ensure all required environment variables are present.
  */
 export function validateEnvs() {
-  const envs = [
-    'JWT_SECRET',
-    'ADMIN_TOKEN',
-    'DIRECTUS_TOKEN',
-    'MAIL_USER',
-    'MAIL_PASSWORD',
-    'MAIL_HOST',
-    'MAIL_PORT',
-    'MAIL_FROM',
-  ];
+  const envs = ['JWT_SECRET', 'DIRECTUS_TOKEN'];
 
   if (!envs.map(checkVariable).every((e) => e)) {
     exit(1);
