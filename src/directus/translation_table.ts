0603c25202e47769b98bbf27b7f22023dc2aaecc
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-10-16 15:33:39 +0200

    refactor: remove imported directus schemas

--- src/directus/translation_table.ts
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-
-export type TranslationTable = { [lang: string]: { [key: string]: string } };
-
-export const TranslationTableContext = React.createContext(
-  {} as { [key: string]: string }
-);
-
-export const useTranslationTable = () => useContext(TranslationTableContext);
