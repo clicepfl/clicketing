2b8ca687b882088c71bf632a65c6dde7fbd845a0
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-10-22 11:32:15 +0200

    chore(dev): add empty DIRECTUS_TOKEN env to Dockerfile

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const DIRECTUS_TOKEN = process.env.DIRECTUS_TOKEN;
 export const SESSION_LIFE = parseInt(process.env.SESSION_LIFE) | (60 * 60 * 24); // 1 day
 
 function checkVariable(name: string) {
-  if (!process.env[name]) {
+  if (process.env[name] === undefined) {
     console.error(`[ERROR] Missing ${name} env variable`);
     return false;
   }
