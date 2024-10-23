a84e385442608ffe822cb0dd375e820ecc7a5b93
Ludovic Mermod <ludovic.mermod@gmail.com>
2024-10-23 12:44:58 +0200

    feat: add sample qrcode to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default async function Page() {
   return (
     <p>
       {JSON.stringify(await directus().request(readSingleton('association')))}
+      <img src="http://localhost/clicketing/api/qrcode?value=89d02ba9-9e4a-402a-b562-b0f3207556c6&size=1024" />
     </p>
   );
 }
