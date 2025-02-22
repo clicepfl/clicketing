4dcd6bb4e4e84498781e10c4911ad453eaa23174
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-02-22 16:41:04 +0100

    feat: add check in actions

--- src/actions/icbd.tsx
+++ src/actions/icbd.tsx
@@ -27,9 +27,7 @@ export async function sendRegistration({
   eventId,
 }) {
   const event = await directus().request(
-    readItem('events', eventId, {
-      fields: ['*'],
-    })
+    readItem('events', eventId, { fields: ['*'] })
   );
 
   const eventRegistration = {
@@ -76,11 +74,7 @@ export async function sendICBDActivitiesRegistrations({
   const noSlotActivitiesRegistrations = activities
     .filter((a) => noSlotActivitiesIDs.includes(a.id))
     .map((a) => {
-      return {
-        icbd_activity: a,
-        registration,
-        attended: false,
-      };
+      return { icbd_activity: a, registration, attended: false };
     });
 
   const activityRegistrations = activities
@@ -105,5 +99,25 @@ export async function sendICBDActivitiesRegistrations({
 export async function completeRegistration(id: string) {
   await directus().request(
     updateItem('registrations', id, { registration_complete: true })
+  );
+}
+
+export async function getRegistrations(eventId: string) {
+  return await directus().request(
+    readItems('registrations', { filter: { event: { _eq: eventId } } })
+  );
+}
+
+export async function getRegistration(id: string) {
+  try {
+    return await directus().request(readItem('registrations', id));
+  } catch (e) {
+    return null;
+  }
+}
+
+export async function checkinRegistration(registrationId: string) {
+  await directus().request(
+    updateItem('registrations', registrationId, { checked_in: true })
   );
 }
