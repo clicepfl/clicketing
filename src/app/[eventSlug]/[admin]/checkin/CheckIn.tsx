ed05ee75cc8986229da207dce9986a4a46ef7214
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-03-16 16:20:06 +0100

    refactor: extract participant display to component

--- src/app/[eventSlug]/[admin]/checkin/CheckIn.tsx
+++ src/app/[eventSlug]/[admin]/checkin/CheckIn.tsx
@@ -1,12 +1,7 @@
 'use client';
 
-import Card from '@/components/Card';
+import ParticipantDisplay from '@/components/ParticipantDisplay';
 import QRScannerSelector from '@/components/QRScannerSelector';
-import SplitText from '@/components/SplitText';
-import CheckCircleIcon from '@/components/icons/CheckCircleIcon';
-import ErrorIcon from '@/components/icons/ErrorIcon';
-import PriceIcon from '@/components/icons/PriceIcon';
-import TicketIcon from '@/components/icons/TicketIcon';
 import { Event, Registration } from '@/types/aliases';
 import { useState } from 'react';
 import { FDCheckinDialog, ICBDCheckinDialog } from './CheckinDialog';
@@ -53,77 +48,6 @@ export function CheckIn({
               return <></>;
           }
         }}
-      />
-    </div>
-  );
-}
-
-function ParticipantDisplay({
-  event,
-  participant,
-}: {
-  event: Event;
-  participant: Registration;
-}) {
-  switch (event.type) {
-    case 'icbd':
-      return <ICBDParticipantDisplay participant={participant} />;
-    case 'faculty_dinner':
-      return <FDParticipantDisplay participant={participant} />;
-    default:
-      break;
-  }
-}
-
-function ICBDParticipantDisplay({
-  participant,
-}: {
-  participant: Registration;
-}) {
-  return (
-    <div className="participant-display">
-      <SplitText
-        snippets={[
-          `${participant.first_name} ${participant.family_name}`,
-          participant.email,
-        ]}
-      />
-
-      {participant.payment !== null ? (
-        participant.checked_in ? (
-          <Card>
-            <CheckCircleIcon className="icon" />
-            Checked in
-          </Card>
-        ) : (
-          <Card>
-            <TicketIcon className="icon" />
-            Deposit made, not checked in
-          </Card>
-        )
-      ) : participant.checked_in ? (
-        <Card>
-          <ErrorIcon className="icon" />
-          Checked in without deposit !!!
-        </Card>
-      ) : (
-        <Card>
-          <PriceIcon className="icon" />
-          Deposit missing !
-        </Card>
-      )}
-    </div>
-  );
-}
-
-function FDParticipantDisplay({ participant }: { participant: Registration }) {
-  return (
-    <div className="participant-display">
-      <SplitText
-        snippets={[
-          `${participant.first_name} ${participant.family_name}`,
-          participant.email,
-        ]}
       />
     </div>
   );
