05d4075ff37eba0c6c6e11b1178025be8ad66a27
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-09-06 16:51:41 +0200

    feat(hello-world): add single checking for teams

--- src/app/[eventSlug]/[admin]/checkin/CheckinDialog.tsx
+++ src/app/[eventSlug]/[admin]/checkin/CheckinDialog.tsx
@@ -1,12 +1,14 @@
 import { checkInRegistration, markPayment } from '@/actions/common';
+import { getTeamMembers } from '@/actions/hello-world';
 import { returnDeposit } from '@/actions/icbd';
 import Card from '@/components/Card';
 import DropdownCard from '@/components/DropdownCard';
 import CheckCircleIcon from '@/components/icons/CheckCircleIcon';
+import ErrorIcon from '@/components/icons/ErrorIcon';
 import PriceIcon from '@/components/icons/PriceIcon';
 import Split from '@/components/Split';
 import { Event, Registration } from '@/types/aliases';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 interface DialogDefaultProps {
   participant: Registration;
@@ -156,34 +158,80 @@ export function HWCheckinDialog({
   participant,
   setParticipants,
 }: { event: Event } & DialogDefaultProps) {
-  const [payment, setPayment] = useState(null);
+  const [teamMembers, setTeamMembers] = useState<Registration[]>([]);
 
-  return (
+  useEffect(() => {
+    getTeamMembers(participant.team, event.id.toString()).then((members) =>
+      setTeamMembers(members)
+    );
+  }, [teamMembers]);
+
+  return teamMembers.length === 0 ? (
+    <>Loading team members...</>
+  ) : (
     <>
-      <Split>
-        <b>{`${participant.first_name} ${participant.family_name}`}</b>
-        <span>{participant.team}</span>
-      </Split>
+      <b>{participant.team}</b>
 
-      {participant.checked_in ? (
+      {teamMembers.length != 3 ? (
+        <>
+          <Card>
+            <ErrorIcon className="icon" />
+            Team {participant.team} has {teamMembers.length} members rather than
+            3 !
+          </Card>
+        </>
+      ) : null}
+
+      {teamMembers.map((member) => (
+        <>
+          <Split>
+            <span className="center-text half">
+              <b>{`${member.first_name} ${member.family_name}`}</b>
+            </span>
+            {member.checked_in ? (
+              <Card key={member.id}>
+                <CheckCircleIcon className="icon" />
+                Already checked in
+              </Card>
+            ) : (
+              <button
+                onClick={async () => {
+                  const newRes = await checkInRegistration(member.id);
+                  setParticipants((value) => [
+                    ...value.filter((p) => p.id != member.id),
+                    newRes,
+                  ]);
+                }}
+                key={member.id}
+              >
+                Check-in {member.first_name} {member.family_name}
+              </button>
+            )}
+          </Split>
+        </>
+      ))}
+
+      {teamMembers.filter((member) => !member.checked_in).length === 0 ? (
         <Card>
           <CheckCircleIcon className="icon" />
-          Already checked in
+          All team members checked in
         </Card>
       ) : (
-        <>
-          <button
-            onClick={async () => {
-              const newRes = await checkInRegistration(participant.id);
-              setParticipants((value) => [
-                ...value.filter((p) => p.id != participant.id),
-                newRes,
-              ]);
-            }}
-          >
-            Check-in
-          </button>
-        </>
+        <button
+          onClick={async () => {
+            for (const member of teamMembers) {
+              if (!member.checked_in) {
+                const newRes = await checkInRegistration(member.id);
+                setParticipants((value) => [
+                  ...value.filter((p) => p.id != member.id),
+                  newRes,
+                ]);
+              }
+            }
+          }}
+        >
+          Check-in all team
+        </button>
       )}
 
       <button onClick={close}>Close</button>
