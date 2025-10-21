5775737a3195801c098ee742fab5d1227f0596e2
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-10-21 14:50:28 +0200

    feat: add basic event form

---
+++ src/components/forms/BasicForm.tsx
@@ -0,0 +1,271 @@
+'use client';
+import {
+  AUTUMN_YEARS,
+  FormStates,
+  ParticipantState,
+  SECTIONS,
+  Season,
+  emptyParticipantState,
+  validateParticipant,
+} from '@/actions/common-client';
+import { sendRegistration } from '@/actions/common-server';
+import { Event } from '@/types/aliases';
+import { ElementType, ReactNode, useState } from 'react';
+import Card from '../Card';
+import CheckboxCard from '../CheckboxCard';
+import DropdownCard from '../DropdownCard';
+import ErrorMessage from '../ErrorMessage';
+import InfoLine from '../InfoLine';
+import LargeTextInputCard from '../LargeTextInputCard';
+import TextInputCard from '../TextInputCard';
+import CalendarIcon from '../icons/CalendarIcon';
+import CheckCircleIcon from '../icons/CheckCircleIcon';
+import EmailIcon from '../icons/EmailIcon';
+import MapPinIcon from '../icons/MapPinIcon';
+import PencilIcon from '../icons/PencilIcon';
+import PriceIcon from '../icons/PriceIcon';
+import TeamIcon from '../icons/TeamIcon';
+import UserIcon from '../icons/UserIcon';
+
+type State = {
+  formState: FormStates;
+  participant: ParticipantState;
+  consent: boolean;
+  errorMessage: string;
+  comments: string;
+};
+
+async function validateValues(s: State, eventId: number) {
+  const error = await validateParticipant(
+    s.participant,
+    eventId,
+    Season.Autumn
+  );
+  if (error) {
+    return error;
+  }
+
+  if (!s.consent) {
+    return 'Consent is required';
+  }
+
+  return null;
+}
+
+export default function HelloWorldForm({
+  event,
+  date,
+  location,
+  deposit,
+}: {
+  event: Event;
+  date: string;
+  location: string;
+  deposit: string;
+}) {
+  // Info items
+  const infoItems: [ElementType, ReactNode][] = [
+    [CalendarIcon, date],
+    [MapPinIcon, location],
+    [PriceIcon, deposit],
+  ];
+
+  // Define initial state
+  const initialState: State = {
+    formState: FormStates.Form,
+    participant: emptyParticipantState,
+    consent: false,
+    errorMessage: '',
+    comments: '',
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const setField = <K extends keyof State>(field: K, value: State[K]) => {
+    setState((prevState) => ({
+      ...prevState,
+      [field]: value,
+    }));
+  };
+
+  const setParticipantField = <K extends keyof ParticipantState>(
+    field: K,
+    value: ParticipantState[K]
+  ) => {
+    setState((prevState) => ({
+      ...prevState,
+      participant: {
+        ...prevState.participant,
+        [field]: value,
+      },
+    }));
+  };
+
+  return (
+    <div className="form">
+      <h1>{event.name}</h1>
+      <InfoLine infoItems={infoItems}></InfoLine>
+
+      {(() => {
+        switch (state.formState) {
+          case FormStates.Form:
+            return (
+              <Form
+                s={state}
+                setField={setField}
+                setParticipantField={setParticipantField}
+                event={event}
+              />
+            );
+          case FormStates.Loading:
+            return <Loading></Loading>;
+          case FormStates.Confirmation:
+            return <Confirmation />;
+          case FormStates.Error:
+            return <ErrorDisplay message={state.errorMessage} />;
+          default:
+            return null;
+        }
+      })()}
+    </div>
+  );
+}
+
+function Form({
+  s,
+  setField,
+  setParticipantField,
+  event,
+}: {
+  s: State;
+  setField: <K extends keyof State>(field: K, value: State[K]) => void;
+  setParticipantField: <K extends keyof ParticipantState>(
+    field: K,
+    value: ParticipantState[K]
+  ) => void;
+  event: Event;
+}) {
+  return (
+    <>
+      <section>
+        <TextInputCard
+          Icon={UserIcon}
+          placeholder="First Name"
+          inputState={{
+            value: s.participant.firstName,
+            setValue: (value) => setParticipantField('firstName', value),
+          }}
+        />
+        <TextInputCard
+          Icon={UserIcon}
+          placeholder="Last Name"
+          inputState={{
+            value: s.participant.lastName,
+            setValue: (value) => setParticipantField('lastName', value),
+          }}
+        />
+        <TextInputCard
+          Icon={EmailIcon}
+          placeholder="EPFL Email"
+          inputState={{
+            value: s.participant.email,
+            setValue: (value) => setParticipantField('email', value),
+          }}
+        />
+
+        <DropdownCard
+          Icon={TeamIcon}
+          placeholder="Section"
+          options={SECTIONS.map((v) => ({
+            display: v,
+            value: v,
+          }))}
+          dropdownState={{
+            value: s.participant.section,
+            setValue: (value) => setParticipantField('section', value),
+          }}
+        />
+
+        <DropdownCard
+          Icon={TeamIcon}
+          placeholder="Year"
+          options={AUTUMN_YEARS.map((v) => ({
+            display: v,
+            value: v,
+          }))}
+          dropdownState={{
+            value: s.participant.year,
+            setValue: (value) => setParticipantField('year', value),
+          }}
+        />
+
+        <CheckboxCard
+          checkboxState={{
+            value: s.consent,
+            setValue: (value) => setField('consent', value),
+          }}
+        >
+          I consent to CLIC taking photographs of me at
+          {event.name} and using them for promotional purposes.
+        </CheckboxCard>
+
+        <LargeTextInputCard
+          Icon={PencilIcon}
+          placeholder="Additional Comments"
+          inputState={{
+            value: s.comments,
+            setValue: (value) => setField('comments', value),
+          }}
+          rows={2}
+        />
+      </section>
+
+      <button
+        onClick={async () => {
+          const error = await validateValues(s, event.id);
+
+          if (error) {
+            setField('errorMessage', error);
+            return;
+          }
+
+          setField('formState', FormStates.Loading);
+
+          try {
+            for (let i = 1; i <= 3; i++) {
+              const member = s[`member${i}`];
+              await sendRegistration({
+                eventId: event.id,
+                participant: s.participant,
+                comments: s.comments,
+              });
+            }
+            setField('formState', FormStates.Confirmation);
+          } catch (error) {
+            setField('errorMessage', error.message);
+            setField('formState', FormStates.Error);
+          }
+        }}
+      >
+        Confirm Registration
+      </button>
+
+      <ErrorMessage message={s.errorMessage}></ErrorMessage>
+    </>
+  );
+}
+
+function Loading({}) {
+  return <p>Loading...</p>;
+}
+
+function Confirmation() {
+  return (
+    <>
+      <Card Icon={CheckCircleIcon}>
+        <p>Your registration to Hello World is successful !</p>
+      </Card>
+      <p>Check your email for confirmation, and see you soon !</p>
+    </>
+  );
+}
