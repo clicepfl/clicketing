c617b661158dd3fa1c4a88ae9af778526b54ac47
Sidonie Bouthors <92856411+SidonieBouthors@users.noreply.github.com>
2026-02-20 14:16:52 +0100

    feat: don't require payment for no interviews (#25)
    * feat: don't require payment for no interviews
    
    * fix: mark payment as not needed
    
    ---------
    
    Co-authored-by: SekoiaTree <51149447+SekoiaTree@users.noreply.github.com>

--- src/actions/icbd.tsx
+++ src/actions/icbd.tsx
@@ -47,10 +47,22 @@ export async function sendICBDActivitiesRegistrations({
   );
 }
 
-export async function completeRegistration(id: string) {
-  await directus().request(
-    updateItem('registrations', id, { registration_complete: true })
-  );
+export async function completeRegistration(id: string, hasInterview: boolean) {
+  if (hasInterview) {
+    await directus().request(
+      updateItem('registrations', id, { registration_complete: true })
+    );
+  } else {
+    await directus().request(
+      updateItem('registrations', id, {
+        registration_complete: true,
+        registration_email_sent: true,
+        payment: 'not-needed',
+        retreived_deposit: true,
+        can_retreive_deposit: false,
+      })
+    );
+  }
 }
 
 export async function returnDeposit(
