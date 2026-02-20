17a489279e5891a6c0195f6826e1744976d8c730
Lucie Mermod <lucie.mermod@gmail.com>
2026-02-20 14:58:13 +0100

    feat: sort activities by name then time

--- src/app/[eventSlug]/icbd.tsx
+++ src/app/[eventSlug]/icbd.tsx
@@ -4,26 +4,37 @@ import { getTranslation } from '@/locales';
 import { Event } from '@/types/aliases';
 
 export default async function ICBD({ event }: { event: Event }) {
-  let db_activities = await getICBDActivities(event.slug);
+  let dbActivities = await getICBDActivities(event.slug);
 
-  let activities = db_activities.map((a) => {
-    if (a.timeslots === null) {
-      return null;
-    }
+  let activities = dbActivities
+    .sort((a, b) => {
+      const at = getTranslation(a, 'en-US');
+      const bt = getTranslation(b, 'en-US');
+      if (at.name == bt.name) {
+        return a.timeslots[0].start_time.localeCompare(
+          b.timeslots[0].start_time
+        );
+      }
+      return at.name.localeCompare(bt.name);
+    })
+    .map((a) => {
+      if (a.timeslots === null) {
+        return null;
+      }
 
-    const infos = getTranslation(a, 'en-US');
-    const time = `${a.timeslots[0].start_time.substring(0, 5)} - ${a.timeslots[0].end_time.substring(0, 5)}`;
-    const room = a.timeslots[0].room;
+      const infos = getTranslation(a, 'en-US');
+      const time = `${a.timeslots[0].start_time.substring(0, 5)} - ${a.timeslots[0].end_time.substring(0, 5)}`;
+      const room = a.timeslots[0].room;
 
-    return {
-      id: a.id,
-      title: infos.name,
-      description: infos.description,
-      type: a.type,
-      time: time,
-      room: room,
-    };
-  });
+      return {
+        id: a.id,
+        title: infos.name,
+        description: infos.description,
+        type: a.type,
+        time: time,
+        room: room,
+      };
+    });
 
   let talks = activities.filter((a) => a !== null && a.type == 'talk');
   let discussions = activities.filter(
